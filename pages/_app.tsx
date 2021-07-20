af8e804768e71292f544b1575c5a4fa306a6be58
jonathanprozzi <jonathanprozzi@gmail.com>
2021-07-20 10:40:18 -0400

    added DefaultSEO to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { DefaultSeo } from "next-seo";
 import { ChakraProvider } from "@chakra-ui/react";
 import "@fontsource/bebas-neue/400.css";
 import "@fontsource/roboto/400.css";
@@ -8,6 +9,28 @@ import theme from "../theme";
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
+      <DefaultSeo
+        title='YAP DAO'
+        description='YAP DAO is a community that provides public relations and communications support to the DeFi world. It is a service marketplace for non-technical people to get work in a technical field.'
+        canonical='https://yapdao.xyz'
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          url: "https://yapdao.xyz",
+          site_name: "YAP DAO",
+          title: "YAP DAO",
+          description:
+            "YAP DAO is a community that provides public relations and communications support to the DeFi world. It is a service marketplace for non-technical people to get work in a technical field.",
+          images: [
+            {
+              url: "/yapdao-logo.png",
+              width: 1200,
+              height: 630,
+              alt: "YAP DAO logo: Y within a globe",
+            },
+          ],
+        }}
+      />
       <SiteLayout>
         <Component {...pageProps} />
       </SiteLayout>
