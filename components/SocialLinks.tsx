e0041c224fd9a7ad695c6829e81c365cd9eebc9a
jonathanprozzi <jonathanprozzi@gmail.com>
2021-08-13 16:18:24 -0400

    added social icons

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,13 +1,23 @@
 import { ButtonGroup, ButtonGroupProps, IconButton } from "@chakra-ui/react";
-import { FaGithub, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaTwitter, FaDiscord, FaMedium } from "react-icons/fa";
 
 const SocialLinks = (props: ButtonGroupProps) => (
   <ButtonGroup variant='ghost' color='white' {...props}>
     <IconButton
       as='a'
-      href='https://github.com/yapdao'
-      aria-label='GitHub'
-      icon={<FaGithub fontSize='20px' />}
+      href='https://twitter.com/YAPDAO_'
+      aria-label='Twitter'
+      icon={<FaTwitter fontSize='20px' />}
+      transition='all ease-in-out .25s'
+      _hover={{
+        color: "brand.blue",
+      }}
+    />
+    <IconButton
+      as='a'
+      href='https://discord.gg/xWGhGjDdT3'
+      aria-label='Discord'
+      icon={<FaDiscord fontSize='20px' />}
       transition='all ease-in-out .25s'
       _hover={{
         color: "brand.blue",
@@ -18,6 +28,26 @@ const SocialLinks = (props: ButtonGroupProps) => (
       href='https://twitter.com/YAPDAO_'
       aria-label='Twitter'
       icon={<FaTwitter fontSize='20px' />}
+      transition='all ease-in-out .25s'
+      _hover={{
+        color: "brand.blue",
+      }}
+    />
+    <IconButton
+      as='a'
+      href='https://medium.com/yap-dao'
+      aria-label='Medium'
+      icon={<FaMedium fontSize='20px' />}
+      transition='all ease-in-out .25s'
+      _hover={{
+        color: "brand.blue",
+      }}
+    />
+    <IconButton
+      as='a'
+      href='https://github.com/yapdao'
+      aria-label='GitHub'
+      icon={<FaGithub fontSize='20px' />}
       transition='all ease-in-out .25s'
       _hover={{
         color: "brand.blue",
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "@fontsource/bebas-neue/400.css";
 import "@fontsource/roboto/400.css";
 import SiteLayout from "../components/SiteLayout";
-
 import theme from "../theme";
 
 const MyApp = ({ Component, pageProps }) => {
